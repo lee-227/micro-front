866de970b496f7f950504cb21608a5005afc174c
XiaoLee <lee_227@163.com>
2023-07-01 17:54:10 +0800

    feat(微前端): vue-child 微前端调整

--- qiankun/vue-child/src/main.ts
+++ qiankun/vue-child/src/main.ts
@@ -2,13 +2,29 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
 
-const app = createApp(App)
+const initQianKun = () => {
+  renderWithQiankun({
+    mount(props) {
+      render(props.container)
+    },
+    bootstrap() {},
+    unmount() {},
+    update() {}
+  })
+}
 
-app.use(createPinia())
-app.use(router)
+const render = (container: HTMLElement | undefined) => {
+  // 如果是在主应用的环境下就挂载主应用的节点，否则挂载到本地
+  const appDom = container ? container : '#app'
+  const app = createApp(App)
+  app.use(createPinia())
+  app.use(router)
+  app.mount(appDom)
+}
 
-app.mount('#app')
+// 判断当前应用是否在主应用中
+qiankunWindow.__POWERED_BY_QIANKUN__ ? initQianKun() : render(undefined)
--- qiankun/vue-child/vite.config.ts
+++ qiankun/vue-child/vite.config.ts
@@ -1,18 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
-
+const { name } = require('./package')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'node:path'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    qiankun('vue-child', { // 微应用名字，与主应用注册的微应用名字保持一致
+      useDevMode: true
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 7002
+  },
+  build: {
+    target: 'esnext',
+    lib: {
+      name: `${name}-[name]`,
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      formats: ['umd']
     }
   }
 })
