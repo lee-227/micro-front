fea42528d9a2453dec06425f935b3f405f052413
XiaoLee <lee_227@163.com>
2023-07-12 23:17:38 +0800

    feat(微前端): micro-app base

--- micro-app/base/src/App.js
+++ micro-app/base/src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Base</p>
+        <div>
+          <a className='App-link' href='/react'>
+            React-child
+          </a>
+          <a className='App-link' href='/vue'>
+            Vue-child
+          </a>
+        </div>
       </header>
     </div>
   );
